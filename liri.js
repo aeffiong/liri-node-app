57f5b08cb3302acd4c9db1c8b3f1128737c0ee2c
aeffiong <aeffiong9@gmail.com>
2018-06-23 00:33:48 -0600

    working on omdb function but getting an error

--- liri.js
+++ liri.js
@@ -14,41 +14,81 @@ var fs = require("fs");
 // create variables to use
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var nodeArgs = process.argv
 var command = process.argv[2];
-var userInput = process.argv[3];
+var userInput = "";
 var twitterName = {
     screen_name: "atim38297792"
 };
 var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
 console.log("LIRI is ready");
-myTweets()
 
-// twitter function
+// create switch statements for the commands for the user to choose from
+switch (command) {
+    case "my-tweets":
+        myTweets();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        random();
+        break;
+    default:
+        "Choose a command: my-tweets, spotify-this-song, movie-this, or do-what-it-say"
+
+}
+
+
+// twitter function - resulting in null :(
 function myTweets() {
-    
-client.get("statuses/user_timeline", twitterName, function(error, tweets, response){
-    if (error);
-    return console.log(error);
-    console.log(tweets);
-    console.log(response);
-})
+
+    client.get("statuses/user_timeline", twitterName, function (error, tweets, response) {
+        if (error);
+        return console.log(error);
+        console.log(tweets);
+        console.log(response);
+    })
 }
 
 // spotify function
-spotify.search({ type: 'track', query: 'Wish I knew You' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+function spotifyThis() {
+    // loop through to have multple words in the song title
+    for (i = 3; i < nodeArgs.length; i++) {
+        userInput += process.argv[i] + "+";
     }
-    console.log("--------------------")
-    console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
-    console.log("Song Title: " + data.tracks.items[0].name);
-    console.log("Preview Link: " + data.tracks.items[0].preview_url);
-    console.log("Album: " + data.tracks.items[0].album.name);
-    console.log("--------------------")
+    spotify.search({ type: 'track', query: userInput }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("--------------------")
+        console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+        console.log("Song Title: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("--------------------")
 
-  });
+    });
 
-// OMDB request function
+}
+
+// OMDB request function - giving a response error
+function movieThis(){
+    // loop through to have multiple words in the movie title
+    for ( i = 3; i < nodeArgs.length; i++) {
+        userInput += process.argv[i] + "+";
+    }
+    request(queryUrl, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            console.log(body);
+            console.log("The movie's release year is: " + JSON.parse(body).Year);
+        }
+    })
+}
+
 
 
