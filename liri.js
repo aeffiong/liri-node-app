326a3dfe9eff7d58a69eb1e4566082b8d7ca9b6e
aeffiong <aeffiong9@gmail.com>
2018-06-23 13:48:44 -0600

    all commands are working

--- liri.js
+++ liri.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 
 // grab the information from keys.js
 var keys = require("./keys.js");
-console.log("hello");
+
 
 // require the needed npm packages
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
+var inquirer = require("inquirer");
 
 // create variables to use
 var spotify = new Spotify(keys.spotify);
@@ -30,7 +31,13 @@ switch (command) {
         myTweets();
         break;
     case "spotify-this-song":
-        spotifyThis();
+    // loop through to have multple words in the song title
+        var songName = "";
+        //node filename.js spotify-this-song wish on this star
+        for (i = 3; i < process.argv.length; i++) {
+            songName += process.argv[i] + "+";
+        }
+        spotifyThis(songName);
         break;
     case "movie-this":
         movieThis();
@@ -47,21 +54,26 @@ switch (command) {
 // twitter function - resulting in null :(
 function myTweets() {
 
-    client.get("statuses/user_timeline", twitterName, function (error, tweets, response) {
-        if (error);
-        return console.log(error);
-        console.log(tweets);
-        console.log(response);
+    client.get("search/tweets", {q: "atim38297792", count: 20} , function (error, tweets, response) {
+        if (error){
+        console.log(error);
+    } else {
+        for(var i =0; i <tweets.statuses.length; i++ ) {
+            console.log(tweets.statuses[i].text + " on " +tweets.statuses[i].created_at);
+        }
+    }
+        // console.log(tweets);
+      
+        //   for loop to get through the status lengths and add the created_at
+     
     })
 }
 
 // spotify function
-function spotifyThis() {
-    // loop through to have multple words in the song title
-    for (i = 3; i < nodeArgs.length; i++) {
-        userInput += process.argv[i] + "+";
-    }
-    spotify.search({ type: 'track', query: userInput }, function (err, data) {
+// function spotifyThis() {
+function spotifyThis(query) {
+    
+    spotify.search({ type: 'track', query: query }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -76,18 +88,41 @@ function spotifyThis() {
 
 }
 
+function makeUrl(answer) {
+    return "http://www.omdbapi.com/?t=" + answer + "&y=&plot=short&apikey=trilogy";
+}
+
 // OMDB request function - giving a response error
 function movieThis(){
-    // loop through to have multiple words in the movie title
-    for ( i = 3; i < nodeArgs.length; i++) {
-        userInput += process.argv[i] + "+";
-    }
-    request(queryUrl, function(error, response, body) {
-        if(!error && response.statusCode === 200) {
-            console.log(body);
-            console.log("The movie's release year is: " + JSON.parse(body).Year);
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Type in your favorite movie",
+            name: "movieResponse"
         }
+    ]).then(function (movieAnswer){
+        console.log(movieAnswer.movieResponse);
+    // loop through to have multiple words in the movie title
+        for ( i = 3; i < nodeArgs.length; i++) {
+            userInput += process.argv[i] + "+";
+        }
+        request(makeUrl(movieAnswer.movieResponse), function(error, response, body) {
+            if(!error && response.statusCode === 200) {
+                console.log("-------------------------------");
+                console.log(JSON.parse(body).Title);
+                console.log("The movie's release year is: " + JSON.parse(body).Year);
+                console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+                console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Produced in: " + JSON.parse(body).Country);
+                console.log("Movie language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("-------------------------------");
+            }
+        })
     })
+   
 }
 
 // do what is says function
@@ -96,8 +131,14 @@ function random() {
         if (error) {
             return console.log(error);
         }
-        random = data.split(",");
-        // for loop next
+
+        var randomChoice = data.split(",");
+        console.log(randomChoice);
+        if(randomChoice[0] === "spotify-this-song") {
+            //"IV LIKEIT"
+            spotifyThis(randomChoice[1]);
+        }
+        
     })
 }
 
