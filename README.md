e4992e52b0ec5370ac046dd6e26100e302cf4625
aeffiong <aeffiong9@gmail.com>
2018-06-24 11:35:26 -0600

    added default song and movie when user input is blank

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 ## LIRI
+
+LIRI is a language interpretation and recognition interface that takes in commands from the command line and displays answers.
+
+Want to know more about your favorite song? Type in spotify-this-song.
+
+Curious about a movie someone told you about? Type movie-this for all the details.
+
+You can even find my most recent tweets using the command my-tweets or do something random by typing do-what-it-says.
+
+### Technologies used
+
+This node application was created with additional npm packages, APIs, and JavaScript.
+The npm packages used were:
+* Twitter
+* Node-Spotify-API
+* Request
+* Inquirer
+* DotEnv
+
+### Built with:
+* Visual Studio Code
--- liri.js
+++ liri.js
@@ -23,7 +23,7 @@ var twitterName = {
 };
 var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
-console.log("LIRI is ready");
+// console.log("LIRI is ready");
 
 // create switch statements for the commands for the user to choose from
 switch (command) {
@@ -31,9 +31,8 @@ switch (command) {
         myTweets();
         break;
     case "spotify-this-song":
-    // loop through to have multple words in the song title
+        // loop through to have multple words in the song title
         var songName = "";
-        //node filename.js spotify-this-song wish on this star
         for (i = 3; i < process.argv.length; i++) {
             songName += process.argv[i] + "+";
         }
@@ -46,33 +45,28 @@ switch (command) {
         random();
         break;
     default:
-        "Choose a command: my-tweets, spotify-this-song, movie-this, or do-what-it-say"
+        console.log("Choose a command to get started: my-tweets, spotify-this-song, movie-this, or do-what-it-says");
 
 }
-
-
 // twitter function - resulting in null :(
 function myTweets() {
-
-    client.get("search/tweets", {q: "atim38297792", count: 20} , function (error, tweets, response) {
-        if (error){
-        console.log(error);
-    } else {
-        for(var i =0; i <tweets.statuses.length; i++ ) {
-            console.log(tweets.statuses[i].text + " on " +tweets.statuses[i].created_at);
+    client.get("search/tweets", { q: "atim38297792", count: 20 }, function (error, tweets, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            for (var i = 0; i < tweets.statuses.length; i++) {
+                console.log(tweets.statuses[i].text + " on " + tweets.statuses[i].created_at);
+            }
         }
-    }
-        // console.log(tweets);
-      
-        //   for loop to get through the status lengths and add the created_at
-     
     })
 }
 
 // spotify function
-// function spotifyThis() {
 function spotifyThis(query) {
-    
+    if (query === "") {
+        query = "Take it to da house"
+        // console.log(query)
+    }
     spotify.search({ type: 'track', query: query }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
@@ -83,62 +77,63 @@ function spotifyThis(query) {
         console.log("Preview Link: " + data.tracks.items[0].preview_url);
         console.log("Album: " + data.tracks.items[0].album.name);
         console.log("--------------------")
-
     });
-
 }
-
+// OMDB request functions
 function makeUrl(answer) {
     return "http://www.omdbapi.com/?t=" + answer + "&y=&plot=short&apikey=trilogy";
 }
 
-// OMDB request function - giving a response error
-function movieThis(){
+function movieThis() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Type in your favorite movie",
-            name: "movieResponse"
-        }
-    ]).then(function (movieAnswer){
-        console.log(movieAnswer.movieResponse);
-    // loop through to have multiple words in the movie title
-        for ( i = 3; i < nodeArgs.length; i++) {
-            userInput += process.argv[i] + "+";
-        }
-        request(makeUrl(movieAnswer.movieResponse), function(error, response, body) {
-            if(!error && response.statusCode === 200) {
-                console.log("-------------------------------");
-                console.log(JSON.parse(body).Title);
-                console.log("The movie's release year is: " + JSON.parse(body).Year);
-                console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-                console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Produced in: " + JSON.parse(body).Country);
-                console.log("Movie language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("-------------------------------");
+        .prompt([
+            {
+                type: "input",
+                message: "Type in your favorite movie",
+                name: "movieResponse"
             }
+        ]).then(function (movieAnswer) {
+            console.log(movieAnswer.movieResponse);
+            // loop through to have multiple words in the movie title
+            for (i = 3; i < nodeArgs.length; i++) {
+                userInput += process.argv[i] + "+";
+            }
+            if (movieAnswer.movieResponse === "") {
+                movieAnswer.movieResponse = "Bridesmaids";
+            }
+            request(makeUrl(movieAnswer.movieResponse), function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    // console.log(JSON.parse(body));
+                    console.log("-------------------------------");
+                    console.log(JSON.parse(body).Title);
+                    console.log("The movie's release year is: " + JSON.parse(body).Year);
+                    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+                    console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Produced in: " + JSON.parse(body).Country);
+                    console.log("Movie language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                    console.log("-------------------------------");
+                }
+
+            })
         })
-    })
-   
+
 }
 
 // do what is says function
 function random() {
-    fs.readFile("random.txt", "utf8", function(error, data){
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
         }
 
         var randomChoice = data.split(",");
         console.log(randomChoice);
-        if(randomChoice[0] === "spotify-this-song") {
-            //"IV LIKEIT"
+        if (randomChoice[0] === "spotify-this-song") {
             spotifyThis(randomChoice[1]);
         }
-        
+
     })
 }
 
